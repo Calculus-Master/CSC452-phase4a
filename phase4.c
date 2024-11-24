ac15c3fa4f15964df174ca3e70450561d4a4ff48
Saptarshi Mallick <sapmallick@gmail.com>
2024-11-23 21:22:41 -0700

    Finished clock & sleep handler

--- phase4.c
+++ phase4.c
@@ -19,17 +19,8 @@ ShadowProcess process_table[MAXPROC];
 ShadowProcess* sleep_queue;
 
 // Helper Functions
-void enqueue_sleeping_process()
-{
 
-}
-
-void dequeue_sleeping_process()
-{
-
-}
-
-void sleep_daemon_process()
+int sleep_daemon_process()
 {
     while(1)
     {
@@ -38,7 +29,7 @@ void sleep_daemon_process()
         waitDevice(USLOSS_CLOCK_DEV, 0, &status);
 
         // Loop through and wakeup any sleeping processes that have reached their target time
-        has_proc = 1;
+        int has_proc = 1;
         while(has_proc)
         {
             ShadowProcess* sleep_proc = sleep_queue;
@@ -46,22 +37,31 @@ void sleep_daemon_process()
             // Stop looping if there are no more sleeping processes
             if(sleep_proc == NULL)
                 has_proc = 0;
-
-            int current_time = currentTime();
-
-            if(sleep_proc->target_time <= current_time)
+            else
             {
-                // Wakeup the process
-                unblockProc(sleep_proc->pid);
+                int current_time = currentTime();
 
-                // Remove from queue and clear process data
-                sleep_queue = sleep_proc->next;
-                memset(sleep_proc, 0, sizeof(ShadowProcess));
+                if(sleep_proc->target_time <= current_time)
+                {
+                    // Wakeup the process
+                    unblockProc(sleep_proc->pid);
+
+                    // Remove from queue and clear process data
+                    sleep_queue = sleep_proc->next;
+                    memset(sleep_proc, 0, sizeof(ShadowProcess));
+                }
+                else // Next process is not ready to wake up, so stop looping
+                    has_proc = 0;
             }
-            else // Next process is not ready to wake up, so stop looping
-                has_proc = 0;
         }
     }
+
+    return 0; // Should never reach this point
+}
+
+int terminal_daemon_process()
+{
+    return 0;
 }
 
 // Syscall Handlers
@@ -74,7 +74,28 @@ void sleep_handler(USLOSS_Sysargs* args)
         args->arg4 = (void*)-1;
     else
     {
+        // Setup shadow process struct
+        ShadowProcess* sleep_proc = &process_table[getpid() % MAXPROC];
+        sleep_proc->pid = getpid();
 
+        // Calculate target time
+        int current_time = currentTime(); // µs
+        sleep_proc->target_time = current_time + seconds * 1000 * 1000;
+
+        // Add to sleep queue
+        if(sleep_queue == NULL)
+            sleep_queue = sleep_proc;
+        else
+        {
+            ShadowProcess* current = sleep_queue;
+            while(current->next != NULL)
+                current = current->next;
+
+            current->next = sleep_proc;
+        }
+
+        // Block the process
+        blockMe();
 
         args->arg4 = (void*)0;
     }
@@ -107,4 +128,5 @@ void phase4_init()
 void phase4_start_service_processes()
 {
     int sleep_daemon_pid = spork("Sleep Daemon Process", sleep_daemon_process, NULL, USLOSS_MIN_STACK, 3);
+    int terminal_daemon_pid = spork("Terminal Daemon Process", terminal_daemon_process, NULL, USLOSS_MIN_STACK, 3);
 }
